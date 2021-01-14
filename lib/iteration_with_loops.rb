2d54218a5416a40ccbadcc8ebbe75c831635c90c
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-14 20:09:39 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,20 @@
 def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  
+  low_temp_each_day = []
+  row_index = 0 
+  while row_index < src.count do 
+      element_index = 0
+      daily_low_element = 100 
+      # i dont know what to equal to the daily low element. 3 of my weeks would be off if I waited one level and set it equal to the src[row_index][element_index] and i would get a full error putting that in this level. but thankfully it passed i guess. help? 
+    while element_index< src[row_index].count do
+       
+          if src[row_index][element_index] < daily_low_element
+        daily_low_element = src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1 
+     low_temp_each_day<< daily_low_element
+end
+p low_temp_each_day
 end
